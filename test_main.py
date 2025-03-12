23529c2088681f6681b13ae31798dd238b1ce29e
Soren0608 <camilosalazar0608@gmail.com>
2025-03-12 01:28:43 -0500

    Pruebas

--- test_main.py
+++ test_main.py
@@ -57,7 +57,16 @@ def test_upload_jobs():
     assert "Datos insertados en la tabla jobs" in response.json()["message"]
 
 def test_upload_employees():
-    data = "id,name,datetime,department_id,job_id\n1,Alice,2021-06-15T12:00:00Z,1,1\n2,Bob,2021-09-20T14:00:00Z,2,2\n"
+    data = """id,name,datetime,department_id,job_id
+    1,Alice,2021-06-15T12:00:00Z,1,1
+    2,Bob,2021-09-20T14:00:00Z,2,2
+    3,Charlie,2021-03-10T10:30:00Z,1,1
+    4,David,2021-07-05T15:45:00Z,1,1
+    5,Eve,2021-11-25T08:20:00Z,2,2
+    6,Frank,2021-04-22T13:10:00Z,2,2
+    7,Grace,2021-12-30T17:05:00Z,1,1
+    8,Hank,2021-01-15T21:50:00Z,1,1
+    """
     response = client.post(
         "/upload/employees",
         files={"file": ("employees.csv", data, "text/csv")}
